f121f700d762e786668290bb5aeef48e6a18d83f
warlock150@gmail.com <email@example.com>
2023-01-18 13:41:08 +0100

    feat: no scroll

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const menu = document.getElementById('menu');
 const burger = document.getElementById('nav-button');
 const backDrop = document.getElementById('backdrop');
+const body = document.getElementById('body');
 const myOpinion = `Верстка соответствует макету. Header и welcome объединены, это допускается и мне так удобно.
 Полос прокрутки нет, адаптивное меню работает плавно. ИМХО заслуживаю максимальной оценки, но решать Вам ;)`;
 
@@ -10,5 +11,6 @@ backDrop.addEventListener('click', showMenu);
 function showMenu() {
     menu.classList.toggle('opened');
     backDrop.classList.toggle('opened');
+    body.classList.toggle('overflow');
 }
 console.log(myOpinion);
